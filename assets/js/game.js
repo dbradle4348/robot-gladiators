15a557a3dad840f76270a6e76e0b5bafda11c8fd
King Julian <daniel.bradley09@gmail.com>
2020-11-28 12:48:19 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,8 +29,11 @@ var fight = function(enemyName) {
     }
   
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+var damage = randomNumber(playerAttack - 3, playerAttack);
+
+enemyHealth = Math.max(0, enemyHealth - damage);
+    
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -49,8 +52,11 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+playerHealth = Math.max(0, playerHealth - damage);
+   
+console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
 
@@ -65,6 +71,8 @@ var fight = function(enemyName) {
   }
 };
 
+
+
 // function to start a new game
 var startGame = function() {
    // reset player stats
@@ -78,16 +86,21 @@ var startGame = function() {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
-
+enemyHealth = randomNumber();
       
   fight(pickedEnemyName);
 
     // if we're not at the last enemy in the array
     if (playerHealth > 0 && i < enemyNames.length - 1) {
-    shop();
-}
+      // ask if player wants to use the store before next round
+      var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+    
+      // if yes, take them to the store() function
+      if (storeConfirm) {
+        shop();
+      }
     }
+
     else {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
@@ -125,7 +138,8 @@ var shop = function() {
   // ask player what they'd like to do
   var shopOptionprompt = window.prompt(
     "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
-  );
+  )
+  };
 
   // use switch to carry out action
 
@@ -166,6 +180,13 @@ switch (shopOptionPrompt) {
       break;
   }
 
+};
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
 };
 
 // start the game when the page loads
